6f71169b0ff9084cec52f49ddc2d5efa1e6d7451
jpc818 <48692974+jpc818@users.noreply.github.com>
2022-05-24 22:44:10 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -137,7 +137,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Louisiana", // (optional)
       "bio": "I did not mean to click the accept button. It was an oopsie",
-      "photo": "/managers/carl culichula.jpg", // square ratio recommended (no larger than 500x500)
+//       "photo": "/managers/carl culichula.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2019, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "no", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
